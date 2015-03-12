d76964d999c8629fa7fab44d7afc57c382d0b9eb
laudix <rehemamagoti@gmail.com>
2015-03-13 00:42:54 +0300

    Created some functions to manupulate card value and rank..

--- src/CardBytes.java
+++ src/CardBytes.java
@@ -2,7 +2,7 @@
 public class CardBytes {
 
 	public static boolean isSpecial(int rank) {
-		if (rank=='2' || rank=='7' || rank==Rank.RANK_JOKA || rank=='k')
+		if (rank==Rank.RANK_2 || rank==Rank.RANK_7 || rank==Rank.RANK_JOKA || rank=='k')
 			return true;
 		else
 			return false;
@@ -10,22 +10,24 @@ public class CardBytes {
 
 	public static int setValue(int rank) {
 		switch (rank) {
-		case 'A':
-			return 1;
-		case 'Q':
-			return 2;
-		case 'K':
-			return 4;
-		case 'J':
-			return 25;
-		case Rank.RANK_JOKA:
-			return 50;
-		case '2' :
-			return 20;
-		case 'T' :
-			return 10;
+		case Rank.RANK_A:return 1;
+		case Rank.RANK_K:return 4;
+		case Rank.RANK_Q:return 2;
+		case Rank.RANK_J:return 25;
+		case Rank.RANK_JOKA:return 50;
+		case Rank.RANK_2:return 20;
+		
+		case Rank.RANK_3:return 3;
+		case Rank.RANK_4:return 4;
+		case Rank.RANK_5:return 5;
+		case Rank.RANK_6:return 6;
+		case Rank.RANK_7:return 7;
+		case Rank.RANK_8:return 8;
+		case Rank.RANK_9:return 9;
+		case Rank.RANK_10:return 10;
+		
 		default:
-			return Integer.parseInt(rank+"");//THIS PART NEEDS....
+			return -99;
 		}
 	}
 
